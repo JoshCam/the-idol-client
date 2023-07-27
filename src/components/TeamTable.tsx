f74140ecef0d9f610cb0d69aa68eb3ed2d2ba1b5
Josh Campden <joshcampden15@gmail.com>
2023-07-27 23:18:28 +0100

    move teamWithoutMembers row

--- src/components/TeamTable.tsx
+++ src/components/TeamTable.tsx
@@ -14,6 +14,7 @@ import { IMember } from "../interfaces/member.interface";
 import React from "react";
 import TeamWithMemberRow from "./TeamWithMemberRow";
 import { deleteTeam, editTeam, viewTeam } from "../methods/teams";
+import TeamWithoutMembersRow from "./TeamWithoutMembersRow";
 
 const teamsAndMembers = {
   teams: [
@@ -76,22 +77,7 @@ const TeamTable = ({ props }: any) => {
               <React.Fragment key={team.teamId}>
                 {membersForTeam.length === 0 ? (
                   // Render the team row with no members
-                  <TableRow>
-                    <TableCell align="center">{team.teamName}</TableCell>
-                    <TableCell align="center">
-                      <Button onClick={() => viewTeam(team.teamId)}>
-                        View
-                      </Button>
-                      <Button onClick={() => editTeam(team.teamId)}>
-                        Edit
-                      </Button>
-                      <Button onClick={() => deleteTeam(team.teamId)}>
-                        Delete
-                      </Button>
-                    </TableCell>
-                    <TableCell align="center">No Members</TableCell>
-                    <TableCell align="center">No Actions Available</TableCell>
-                  </TableRow>
+                  <TeamWithoutMembersRow team={team} />
                 ) : (
                   // Render team rows with members
                   membersForTeam.map((member: IMember) => (
